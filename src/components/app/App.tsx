6ad648066514b0fdce02559b58871b3672dab344
michaelsmorgon <smorgon@int.pl>
2025-07-23 14:52:54 +0200

    feat: convert app to functional component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,41 +1,29 @@
 import styles from './App.module.css';
-import React, { type ReactNode } from 'react';
 import SearchSection from '../search-section/SearchSection.tsx';
 import SearchResult from '../search-result/SearchResult.tsx';
 import ErrorBoundary from '../error-boundary/ErrorBoundary.tsx';
 import { LS_SEARCH_ROW } from '../../utils/constants.ts';
+import { useState } from 'react';
 
-type AppState = {
-  searchText: string;
-};
+export default function App() {
+  const [searchText, setSearchText] = useState(
+    localStorage.getItem(LS_SEARCH_ROW) || ''
+  );
 
-class App extends React.Component<Record<string, never>, AppState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    const searchText = localStorage.getItem(LS_SEARCH_ROW) || '';
-    this.state = {
-      searchText,
-    };
-  }
-
-  handleSearch = (searchText: string): void => {
-    this.setState({ searchText });
+  const handleSearch = (searchText: string): void => {
+    setSearchText(searchText);
   };
 
-  render(): ReactNode {
-    return (
-      <>
-        <div className={styles.app_wrapper}>
-          <div className={styles.main}>
-            <ErrorBoundary>
-              <SearchSection onSearch={this.handleSearch} />
-              <SearchResult searchText={this.state.searchText} />
-            </ErrorBoundary>
-          </div>
+  return (
+    <>
+      <div className={styles.app_wrapper}>
+        <div className={styles.main}>
+          <ErrorBoundary>
+            <SearchSection onSearch={handleSearch} />
+            <SearchResult searchText={searchText} />
+          </ErrorBoundary>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
-export default App;
