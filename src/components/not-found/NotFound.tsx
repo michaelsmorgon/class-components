fecb80fc33fa445edd7aa63a5a4d4669e5a08119
michaelsmorgon <smorgon@int.pl>
2025-07-27 13:11:20 +0200

    fix: rename css class

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -3,7 +3,7 @@ import notFoundImage from '../../assets/not-found.svg';
 
 export default function NotFound() {
   return (
-    <div className={styles.notFound}>
+    <div className={styles.not_found}>
       <div className={styles.wrapper}>
         <img src={notFoundImage} alt="Page not found" />
         <p className={styles.message}>Sorry, smth went wrong((</p>
