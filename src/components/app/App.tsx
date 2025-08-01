4ca5d5a9ece2596b3482ab07e9ce78d6936ff933
michaelsmorgon <smorgon@int.pl>
2025-08-01 12:21:10 +0200

    feat: add toggle theme

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,18 +4,21 @@ import AboutPage from '../about-page/AboutPage.tsx';
 import MainLayout from '../main-layout/MainLayout.tsx';
 import MainContent from '../main-content/MainContent.tsx';
 import NotFound from '../not-found/NotFound.tsx';
+import { ThemeProvider } from '../../context/ThemeContext.tsx';
 
 export default function App() {
   return (
-    <div className={styles.app_wrapper}>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<MainContent />} />
-          <Route path="details/:id" element={<MainContent />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <div className={styles.app_wrapper}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<MainContent />} />
+            <Route path="details/:id" element={<MainContent />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,12 +1,32 @@
 import { NavLink } from 'react-router-dom';
+import styles from './Menu.module.css';
+import { useContext, useEffect } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
 
 export default function Menu() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark-theme', theme === 'dark');
+  }, [theme]);
+
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
-    <nav>
-      <NavLink to="." end>
-        Home
-      </NavLink>
-      <NavLink to="about">About</NavLink>
+    <nav className={styles.nav}>
+      <div className={styles.links}>
+        <NavLink to="." end className={styles.link}>
+          Home
+        </NavLink>
+        <NavLink to="about" className={styles.link}>
+          About
+        </NavLink>
+      </div>
+      <button className={styles.themeToggleBtn} onClick={handleClick}>
+        {theme === 'dark' ? '🌙' : '🌞'}
+      </button>
     </nav>
   );
 }
