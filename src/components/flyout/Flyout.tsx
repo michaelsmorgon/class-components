c58fa87cb9cb8a2371f1141c0da03f5e79159b82
michaelsmorgon <smorgon@int.pl>
2025-08-03 23:32:24 +0200

    feat: add flyout wrapper

--- src/components/flyout/Flyout.tsx
+++ src/components/flyout/Flyout.tsx
@@ -13,22 +13,24 @@ export default function Flyout({
 }: Props) {
   return (
     <div className={styles.flyout}>
-      <div className={styles.selectedText}>
-        {selectCount} {selectCount === 1 ? 'item is' : 'items are'} selected
-      </div>
-      <div className={styles.buttonGroup}>
-        <button
-          className={`${styles.button} ${styles.buttonUnselect}`}
-          onClick={onUnselectAll}
-        >
-          Unselect All
-        </button>
-        <button
-          className={`${styles.button} ${styles.buttonDownload}`}
-          onClick={onDownload}
-        >
-          Download
-        </button>
+      <div className={styles.flyoutInner}>
+        <div className={styles.selectedText}>
+          {selectCount} {selectCount === 1 ? 'item is' : 'items are'} selected
+        </div>
+        <div className={styles.buttonGroup}>
+          <button
+            className={`${styles.button} ${styles.buttonUnselect}`}
+            onClick={onUnselectAll}
+          >
+            Unselect All
+          </button>
+          <button
+            className={`${styles.button} ${styles.buttonDownload}`}
+            onClick={onDownload}
+          >
+            Download
+          </button>
+        </div>
       </div>
     </div>
   );
